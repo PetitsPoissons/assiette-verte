62a0db9ad825961ed86f0863f5506d3701b418c4
Sandrine Poissonnet <petitspoissons@icloud.com>
2021-01-27 11:40:34 -0800

    readme update

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # assiette-verte
+
+## INSTALL INSTRUCTIONS
+
+At the command line:
+
+```
+cd client
+npm install
+```
+
+Then to start the app:
+
+```
+npm start
+```
--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class SearchBar extends React.Component {
+const SearchBar = () => {
   state = { term: '' };
 
   onInputChange = (event) => {
