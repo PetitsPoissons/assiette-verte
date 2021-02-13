fc83a2cae8544959be696eb771f421f4d43b96fe
Sandrine Poissonnet <petitspoissons@icloud.com>
2021-02-12 20:33:37 -0800

    google authentication

--- server/index.js
+++ server/index.js
@@ -1,9 +1,36 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+// tell passport library that it should make use of the Google strategy inside our app
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('accessToken', accessToken);
+      console.log('refreshToken', refreshToken);
+      console.log('profile', profile);
+    }
+  )
+);
+
+// pass user off to passport when they hit that endpoint so they are getting passed on to the authentication flow
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  })
+);
+
+// route handler once user is sent back to callback route, with the google code
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
