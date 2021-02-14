202c68b814430d613de61f50b3e830ec04d56e52
Sandrine Poissonnet <petitspoissons@icloud.com>
2021-02-13 22:15:20 -0800

    adding cookies

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -9,6 +11,18 @@ mongoose.connect(keys.mongoURI);
 
 // create our express app
 const app = express();
+
+// tell express that it needs to use cookies
+app.use(
+  cookieSession({
+    maxAge: 6 * 60 * 60 * 1000, // cookie lasts 6 hrs in milliseconds
+    keys: [keys.cookieKey], // key used to encrypt our cookie
+  })
+);
+
+// instruct passport that it needs to make use of cookies to handle authentication in our app
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
