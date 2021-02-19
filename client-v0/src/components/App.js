2a1eaac094ed3bcd3780b1fbfa06ab77ff552aee
Sandrine Poissonnet <petitspoissons@icloud.com>
2021-02-19 12:06:05 -0800

    scaffolding for recipes components

--- client-v0/src/components/App.js
+++ client-v0/src/components/App.js
@@ -1,12 +1,1 @@
-import React from 'react';
-import SearchSpoonacular from './SearchSpoonacular';
 
-const App = () => {
-  return (
-    <div className="ui container">
-      <SearchSpoonacular />
-    </div>
-  );
-};
-
-export default App;
