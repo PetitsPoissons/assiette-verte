956efe189d0fda8dbb57679ad128dc87db5a22fe
Sandrine Poissonnet <petitspoissons@icloud.com>
2021-03-05 08:08:15 -0800

    adding proxy for http requests

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './Header';
 // import SearchSpoonacular from './SearchSpoonacular';
 import RecipeCreate from './recipes/RecipeCreate';
 import RecipeDelete from './recipes/RecipeDelete';
@@ -9,13 +10,12 @@ import RecipeShow from './recipes/RecipeShow';
 
 const App = () => {
   return (
-    <div>
-      {/* <div className="ui container">
-        <p>Hi there</p>
-        <a href="/auth/google">Sign in with Google</a>
-      </div> */}
+    <div className="ui container">
+      {/* <p>Hi there</p>
+        <a href="/auth/google">Sign in with Google</a> */}
       <BrowserRouter>
         <div>
+          <Header />
           <Route path="/" exact component={RecipeList} />
           <Route path="/recipes/new" exact component={RecipeCreate} />
           <Route path="/recipes/edit" exact component={RecipeEdit} />
